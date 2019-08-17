011e189aea628080dec098ebcd2406a380ad3309
Subilan <suyuanliang1995@gmail.com>
2019-08-18 00:20:27 +0800

    ⚡ Add powerful plugins

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,5 +18,17 @@ module.exports = {
         search: true,
         searchMaxSuggestions: 10
     },
-    dest: "./public"
+    dest: "./public",
+    plugins: {
+        "@vuepress/last-updated": {
+            transformer: (timestamp) => {
+                const moment = require("moment");
+                moment.locale("zh-CN");
+                return moment(timestamp).fromNow();
+            }
+        },
+        "@vuepress/medium-zoom": {
+            selector: ".theme-default-content :not(a) > img",
+        },
+    },
 }
