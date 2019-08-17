b63aaff6a2a37784638e08b99b43d8730f9edfe1
Subilan <suyuanliang1995@gmail.com>
2019-08-17 17:18:34 +0800

    Theme Configuration

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,17 +1,17 @@
 module.exports = {
     title: "Subilan's Blog",
     description: "再次重演鸽子奇迹",
-    serviceWorker: true,
-    theme: "simple-blog",
     themeConfig: {
-        author: "Subilan's Blog",
-        navbar: {
-            "Friends": "/friends/",
-            "About": "/about/",
-        },
-        copyrightText: "如无特殊说明，所有内容使用 MIT 许可协议",
-        year: "2019",
-
-    },
-    plugins: ["@vuepress/blog"]
+        nav: [
+            { text: "Blog", link: "/" },
+            { text: "Friends", link: "/friends" },
+            { text: "About", link: "/about" },
+        ],
+        lastUpdated: "最后更新于",
+        hiddenPages: [
+            "/",
+            "/About.html",
+            "/Friends.html",
+        ]
+    }
 }
