797690572ce8bf6d191df2751a4f7439dc446536
Subilan <suyuanliang1995@gmail.com>
2020-08-01 00:01:32 +0800

    Word count basic

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -1,187 +1,146 @@
 <template>
-  <div
-    class="theme-container"
-    :class="pageClasses"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
-  >
-    <Navbar
-      v-if="shouldShowNavbar"
-      @toggle-sidebar="toggleSidebar"
-    />
+	<div class="theme-container" :class="pageClasses" @touchstart="onTouchStart" @touchend="onTouchEnd">
+		<Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
 
-    <div
-      class="sidebar-mask"
-      @click="toggleSidebar(false)"
-    ></div>
+		<div class="sidebar-mask" @click="toggleSidebar(false)"></div>
 
-    <Sidebar
-      :items="sidebarItems"
-      @toggle-sidebar="toggleSidebar"
-    >
-      <slot
-        name="sidebar-top"
-        slot="top"
-      />
-      <slot
-        name="sidebar-bottom"
-        slot="bottom"
-      />
-    </Sidebar>
-    <div class="post-list" v-if="isRoot">
-      <div class="post" v-for="(post, i) of getPages()">
-         <router-link class="post-title" :to="post.path">{{ post.title }}</router-link>
-         <!--<span v-if="isPublishDate(post, getPages(), i)" class="edited-text">edited</span>-->
-         <!--<span class="last-updated">{{ ( isPublishDate(post, getPages(), i) ? "编辑于 " : "" ) + getFormatedDate(post.lastUpdated) }}</span>-->
-         <span class="last-updated">{{ post.frontmatter.date !== undefined ? post.frontmatter.date : getFormatedDate(post.lastUpdated) }}</span>
-         <span class="post-content" v-html="'<p>' + post.frontmatter.desc + '</p>'"></span>
-         <div class="post-actions">
-          <router-link class="continue-reading" :to="post.path">继续阅读 &raquo;</router-link>
-         </div>
-      </div>
-    </div>
+		<Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
+			<slot name="sidebar-top" slot="top" />
+			<slot name="sidebar-bottom" slot="bottom" />
+		</Sidebar>
+		<div class="post-list" v-if="isRoot">
+			<div class="post" v-for="(post, i) of getPages()">
+				<router-link class="post-title" :to="post.path">{{ post.title }}</router-link>
+				<!--<span v-if="isPublishDate(post, getPages(), i)" class="edited-text">edited</span>-->
+				<!--<span class="last-updated">{{ ( isPublishDate(post, getPages(), i) ? "编辑于 " : "" ) + getFormatedDate(post.lastUpdated) }}</span>-->
+				<span class="last-updated">{{ post.frontmatter.date !== undefined ? post.frontmatter.date : getFormatedDate(post.lastUpdated) }} · 约 {{ countWords(post.content) }} 字</span>
+				<span class="post-content" v-html="'<p>' + post.frontmatter.desc + '</p>'"></span>
+				<div class="post-actions">
+					<router-link class="continue-reading" :to="post.path">继续阅读 &raquo;</router-link>
+				</div>
+			</div>
+		</div>
 
-    <Page
-      v-else
-      :sidebar-items="sidebarItems"
-    >
-      <slot
-        name="page-top"
-        slot="top"
-      />
-      <slot
-        name="page-bottom"
-        slot="bottom"
-      />
-    </Page>
+		<Page v-else :sidebar-items="sidebarItems">
+			<slot name="page-top" slot="top" />
+			<slot name="page-bottom" slot="bottom" />
+		</Page>
 
-    <Footer/>
-  </div>
+		<Footer />
+	</div>
 </template>
 
 <script>
-import Navbar from '@theme/components/Navbar.vue'
-import Page from '@theme/components/Page.vue'
-import Sidebar from '@theme/components/Sidebar.vue'
-import Footer from "../components/Footer.vue"
-import { resolveSidebarItems, getFormatedDate } from '../util'
+import Navbar from "@theme/components/Navbar.vue";
+import Page from "@theme/components/Page.vue";
+import Sidebar from "@theme/components/Sidebar.vue";
+import Footer from "../components/Footer.vue";
+import { resolveSidebarItems, getFormatedDate } from "../util";
 
 export default {
-  components: { Page, Sidebar, Navbar, Footer },
+	components: { Page, Sidebar, Navbar, Footer },
 
-  data () {
-    return {
-      isSidebarOpen: false,
-      max: this.getPageCount
-    }
-  },
+	data() {
+		return {
+			isSidebarOpen: false,
+			max: this.getPageCount
+		};
+	},
 
-  computed: {
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
-        return false
-      }
-      return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
-      )
-    },
+	computed: {
+		shouldShowNavbar() {
+			const { themeConfig } = this.$site;
+			const { frontmatter } = this.$page;
+			if (frontmatter.navbar === false || themeConfig.navbar === false) {
+				return false;
+			}
+			return this.$title || themeConfig.logo || themeConfig.repo || themeConfig.nav || this.$themeLocaleConfig.nav;
+		},
 
-    shouldShowSidebar () {
-      const { frontmatter } = this.$page
-      return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
-      )
-    },
+		shouldShowSidebar() {
+			const { frontmatter } = this.$page;
+			return !frontmatter.home && frontmatter.sidebar !== false && this.sidebarItems.length;
+		},
 
-    sidebarItems () {
-      return resolveSidebarItems(
-        this.$page,
-        this.$page.regularPath,
-        this.$site,
-        this.$localePath
-      )
-    },
+		sidebarItems() {
+			return resolveSidebarItems(this.$page, this.$page.regularPath, this.$site, this.$localePath);
+		},
 
-    pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
-      return [
-        {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
-        },
-        userPageClass
-      ]
-    },
-      
-    isRoot() {
-      const path = this.$route.path;
-      return path === "/";
-    },
-  },
+		pageClasses() {
+			const userPageClass = this.$page.frontmatter.pageClass;
+			return [
+				{
+					"no-navbar": !this.shouldShowNavbar,
+					"sidebar-open": this.isSidebarOpen,
+					"no-sidebar": !this.shouldShowSidebar
+				},
+				userPageClass
+			];
+		},
 
-  mounted () {
-    this.$router.afterEach(() => {
-      this.isSidebarOpen = false
-    });
-  },
+		isRoot() {
+			const path = this.$route.path;
+			return path === "/";
+		}
+	},
 
-  methods: {
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-    },
+	mounted() {
+		this.$router.afterEach(() => {
+			this.isSidebarOpen = false;
+		});
+	},
 
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
+	methods: {
+		toggleSidebar(to) {
+			this.isSidebarOpen = typeof to === "boolean" ? to : !this.isSidebarOpen;
+		},
 
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
-        }
-      }
-    },
+		// side swipe
+		onTouchStart(e) {
+			this.touchStart = {
+				x: e.changedTouches[0].clientX,
+				y: e.changedTouches[0].clientY
+			};
+		},
 
-    getPages() {
-      let p = this.$site.pages.filter(i => !this.$site.themeConfig.hiddenPages.includes(i.path));
-      return p.sort((a, b) => {
-        return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
-      })
-    },
+		onTouchEnd(e) {
+			const dx = e.changedTouches[0].clientX - this.touchStart.x;
+			const dy = e.changedTouches[0].clientY - this.touchStart.y;
+			if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+				if (dx > 0 && this.touchStart.x <= 80) {
+					this.toggleSidebar(true);
+				} else {
+					this.toggleSidebar(false);
+				}
+			}
+		},
 
-    getExcerptContent(excerpt) {
-      return /^<h1.*?>(.|\n)*?<\/h1>((.|\n)*?)$/.exec(excerpt)[2];
-    },
+		getPages() {
+			let p = this.$site.pages.filter(i => !this.$site.themeConfig.hiddenPages.includes(i.path));
+			return p.sort((a, b) => {
+				return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
+			});
+		},
 
-    getFormatedDate,
+		getExcerptContent(excerpt) {
+			return /^<h1.*?>(.|\n)*?<\/h1>((.|\n)*?)$/.exec(excerpt)[2];
+		},
 
-    isPublishDate(current, all_pages, index) {
-      if (all_pages[index-1] == undefined) return "";
-      let c = new Date(current.lastUpdated).getTime();
-      let n = new Date(all_pages[index-1].lastUpdated).getTime();
-      return c > n;
-    }
-  },
-}
+		getFormatedDate,
+
+		isPublishDate(current, all_pages, index) {
+			if (all_pages[index - 1] == undefined) return "";
+			let c = new Date(current.lastUpdated).getTime();
+			let n = new Date(all_pages[index - 1].lastUpdated).getTime();
+			return c > n;
+		},
+
+		countWords(str) {
+			var matches = str.match(/[\u00ff-\uffff]|\S+/g);
+			return matches ? matches.length : 0;
+		}
+	}
+};
 </script>
 
 <style lang="stylus" scoped>
