3460bfff37bd771d3f571b9e5d3233bca095d4cc
Subilan <suyuanliang1995@gmail.com>
2023-07-15 11:42:16 +0800

    delete pages.ts

--- src/pages.ts
+++
@@ -1,1 +0,0 @@
-const data: Record<string, Page> = {"76de1484-e3ae-55cc-86aa-e30a285baf31":{"title":"关于","filename":"About"},"e612ac32-c268-59bc-b1ba-e0b342dda95b":{"title":"联系","filename":"Contact"},"1c25dfca-c2bb-50a7-a0fc-0e9a60a76b06":{"title":"友链","filename":"Friends"},"7b231c22-cce7-5469-93a2-de8695c76080":{"title":"PGP 公钥","filename":"PGP"}}; export default data;
