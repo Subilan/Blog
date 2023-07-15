98907103c3f82c7416804a1ea776b0ce4c1ff4e8
Subilan <suyuanliang1995@gmail.com>
2023-07-15 18:36:22 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 
 <style lang="less">
 .container {
-  margin: calc(1.5rem + 2 * 0.7rem) auto 0 auto;
+  margin: calc(1.5rem + 2 * 1rem) auto 0 auto;
 
   max-width: 740px;
   display: block;
