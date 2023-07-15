3df4af22f4361a6b6415d4920a0a3efab8d65ba5
Subilan <suyuanliang1995@gmail.com>
2023-07-15 11:42:23 +0800

    global component

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './main.less'
 import router from "@/router";
+import Blogroll from "@/components/Blogroll.vue";
+import ExampleLink from "@/components/ExampleLink.vue";
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+    .component("blogroll", Blogroll)
+    .component("example-link", ExampleLink)
+    .use(router)
+    .mount('#app');
