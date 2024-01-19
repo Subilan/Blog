9aefef3b34e9f3c09d2b1e0e434c014a8f18b90a
Subilan <suyuanliang1995@gmail.com>
2024-01-19 16:50:28 +0800

    add vercel insights

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
     <router-view/>
   </div>
   <Footer/>
+  <SpeedInsights/>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
 import Footer from "@/components/Footer.vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <style lang="less">
