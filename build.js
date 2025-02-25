ad1b1df723a252cd2880e786a6d17e566cb71007
Subilan <suyuanliang1995@gmail.com>
2025-02-25 23:37:43 +0800

    fix & add `thought` block

--- build.js
+++ build.js
@@ -270,7 +270,8 @@ async function render(content) {
     const match1 = Array.from(content.matchAll(/:::\s?tip([\S\s]*?):::/g));
     const match2 = Array.from(content.matchAll(/:::\s?warning([\S\s]*?):::/g));
     const match3 = Array.from(content.matchAll(/:::\s?danger([\S\s]*?):::/g));
-    const targetMatch = ['tip', 'warning', 'danger'];
+    const match4 = Array.from(content.matchAll(/:::\s?thought([\S\s]*?):::/g));
+    const targetMatch = ['tip', 'warning', 'danger', 'thought'];
     const targetMatchText = [
         {
             icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>information-outline</title><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" /></svg>',
@@ -283,10 +284,14 @@ async function render(content) {
         {
             icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-octagon-outline</title><path d="M8.27,3L3,8.27V15.73L8.27,21H15.73C17.5,19.24 21,15.73 21,15.73V8.27L15.73,3M9.1,5H14.9L19,9.1V14.9L14.9,19H9.1L5,14.9V9.1M11,15H13V17H11V15M11,7H13V13H11V7" /></svg>',
             name: '特别注意'
-        }
+        },
+        {
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thought-bubble</title><path d="M3.5,19A1.5,1.5 0 0,1 5,20.5A1.5,1.5 0 0,1 3.5,22A1.5,1.5 0 0,1 2,20.5A1.5,1.5 0 0,1 3.5,19M8.5,16A2.5,2.5 0 0,1 11,18.5A2.5,2.5 0 0,1 8.5,21A2.5,2.5 0 0,1 6,18.5A2.5,2.5 0 0,1 8.5,16M14.5,15C13.31,15 12.23,14.5 11.5,13.65C10.77,14.5 9.69,15 8.5,15C6.54,15 4.91,13.59 4.57,11.74C3.07,11.16 2,9.7 2,8A4,4 0 0,1 6,4C6.26,4 6.5,4.03 6.77,4.07C7.5,3.41 8.45,3 9.5,3C10.69,3 11.77,3.5 12.5,4.35C13.23,3.5 14.31,3 15.5,3C17.46,3 19.09,4.41 19.43,6.26C20.93,6.84 22,8.3 22,10A4,4 0 0,1 18,14L17.23,13.93C16.5,14.59 15.55,15 14.5,15Z" /></svg>',
+            name: '想法'
+        }  
     ];
 
-    [match1, match2, match3].forEach((x, i) => {
+    [match1, match2, match3, match4].forEach((x, i) => {
         x.forEach(y => {
             content = content.replace(y[0], `<div class="notice card ${targetMatch[i]}"><div class="notice-title">${targetMatchText[i].icon}${targetMatchText[i].name}</div>${md.render(y[1])}</div>`);
         });
