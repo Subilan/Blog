5290dab28487ee762ed9c4240f50316f326f36b7
Subilan <suyuanliang1995@gmail.com>
2025-08-13 16:43:41 +0800

    optimize back-btn ux

--- components/back-btn.vue
+++ components/back-btn.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="icon-btn topleft noprint" @click="useRouter().go(-1)">
+    <div class="icon-btn topleft noprint" @click="goBack">
         <icon :path="mdiArrowLeft" />
     </div>
 </template>
 
 <script setup lang="ts">
-import { mdiArrowLeft } from '@mdi/js';
+    import { mdiArrowLeft } from '@mdi/js';
+
+    const route = useRoute();
+    const router = useRouter();
+
+    function goBack() {
+        if (route.name) {
+            if (/(posts|pages)/.test(route.name.toString())) {
+                router.push('/')
+                return;
+            }
+        }
+
+        router.go(-1);
+    }
 </script>
